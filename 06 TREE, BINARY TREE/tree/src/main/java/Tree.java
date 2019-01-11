4ce860e9ea77ec8af9d1e9143939386307619b20
Vasil Atanasov <vas.atanasov@gmail.com>
2019-01-11 12:41:18 +0200

    added Tree implementation

--- 06 TREE, BINARY TREE/tree/src/main/java/Tree.java
+++ 06 TREE, BINARY TREE/tree/src/main/java/Tree.java
@@ -1,26 +1,69 @@
+import java.util.*;
 import java.util.function.Consumer;
 
 public class Tree<T> {
 
+    private static final String INDENT = " ";
+
+    private T value;
+    private List<Tree<T>> children;
+
+    public Tree(T value) {
+        this.value = value;
+        this.children = new ArrayList<>();
+    }
+
     @SafeVarargs
     public Tree(T value, Tree<T>... children) {
+        this(value);
+        Collections.addAll(this.children, children);
     }
 
     // append output to builder
     public String print(int indent, StringBuilder builder) {
-        throw new UnsupportedOperationException();
+        for (int i = 0; i < indent * 2; i++) {
+            builder.append(INDENT);
+        }
+        builder.append(this.value)
+                .append("\n");
+        for (Tree<T> child : this.children) {
+            child.print(indent + 1, builder);
+        }
+        return builder.toString();
     }
 
     public void each(Consumer<T> consumer) {
-        throw new UnsupportedOperationException();
+        consumer.accept(this.value);
+        for (Tree<T> child : this.children) {
+            child.each(consumer);
+        }
     }
 
     public Iterable<T> orderDFS() {
-        throw new UnsupportedOperationException();
+        List<T> result = new ArrayList<>();
+        this.DFS(this, result);
+        return result;
+    }
+
+    private void DFS(Tree<T> tree, List<T> result) {
+        for (Tree<T> child : tree.children) {
+            this.DFS(child, result);
+        }
+        result.add(tree.value);
     }
 
     public Iterable<T> orderBFS() {
-        throw new UnsupportedOperationException();
+        List<T> result = new ArrayList<>();
+        Deque<Tree<T>> queue = new ArrayDeque<>();
+        queue.addLast(this);
+        while (! queue.isEmpty()) {
+            Tree<T> node = queue.removeFirst();
+            result.add(node.value);
+            for (Tree<T> child : node.children) {
+                queue.addLast(child);
+            }
+        }
+        return result;
     }
 
 }
