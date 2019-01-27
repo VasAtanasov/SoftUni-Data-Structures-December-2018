78ac21a8b078b1f55b078e19be2f5676f9b28015
Vasil Atanasov <vas.atanasov@gmail.com>
2019-01-27 11:27:54 +0200

    :o: :heavy_check_mark: CircularQueue implementation

--- 04 STACKS AND QUEUES/circularqueue/src/main/java/CircularQueue.java
+++ 04 STACKS AND QUEUES/circularqueue/src/main/java/CircularQueue.java
@@ -28,60 +28,57 @@ public class CircularQueue<E> {
     }
 
     public void enqueue(E element) {
-        if (this.size >= this.elements.length) {
+        if (this.size == this.elements.length) {
             this.grow();
         }
-        this.elements[this.endIndex] = element;
+        this.elements[endIndex] = element;
         this.endIndex = (this.endIndex + 1) % this.elements.length;
         this.size++;
     }
 
-    @SuppressWarnings("unchecked")
-    private void grow() {
-        E[] newElements = (E[]) new Object[this.elements.length * 2];
-        this.copyAllElementsTo(newElements);
-        this.elements = newElements;
-        this.startIndex = 0;
-        this.endIndex = this.size;
-    }
-
-    private void copyAllElementsTo(E[] newElements) {
-        int sourceIndex = this.startIndex;
-        int destinationIndex = 0;
-        for (int i = 0; i < this.size; i++) {
-            newElements[destinationIndex] = this.elements[sourceIndex];
-            sourceIndex = (sourceIndex + 1) % this.elements.length;
-            destinationIndex++;
-        }
-    }
-
     public E dequeue() {
         if (this.size == 0) {
-            throw new IllegalArgumentException("The queue is empty!");
+            throw new IllegalArgumentException();
         }
-        E result = this.elements[this.startIndex];
+        E element = this.elements[startIndex];
         this.startIndex = (this.startIndex + 1) % this.elements.length;
+        this.size--;
         this.size--;
         if (this.size < this.elements.length / 4) {
             this.shrink();
         }
-        return result;
-    }
-
-    @SuppressWarnings("unchecked")
-    private void shrink() {
-        E[] newArray = (E[]) new Object[this.elements.length / 2];
-        this.copyAllElementsTo(newArray);
-        this.startIndex = 0;
-        this.endIndex = this.size;
-        this.elements = newArray;
+        return element;
     }
 
     @SuppressWarnings("unchecked")
     public E[] toArray() {
         E[] resultArray = (E[]) new Object[this.size];
-        this.copyAllElementsTo(resultArray);
+        this.copyElements(resultArray);
         return resultArray;
     }
 
+    @SuppressWarnings("unchecked")
+    private void grow() {
+        E[] newArray = (E[]) new Object[this.elements.length * 2];
+        this.copyElements(newArray);
+        this.elements = newArray;
+        this.startIndex = 0;
+        this.endIndex = this.size;
+    }
+
+    private void copyElements(E[] newArray) {
+        for (int destination = 0, sourceIndex = this.startIndex; destination < this.size; destination++) {
+            newArray[destination] = this.elements[sourceIndex];
+            sourceIndex = (sourceIndex + 1) % this.elements.length;
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    private void shrink() {
+        E[] newArray = (E[]) new Object[this.elements.length / 2];
+        this.copyElements(newArray);
+        this.startIndex = 0;
+        this.endIndex = this.size;
+        this.elements = newArray;
+    }
 }
